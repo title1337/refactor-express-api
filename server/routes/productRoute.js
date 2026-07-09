0872f31523dc52e55d9314009e15c0dd686d4375
Tle <title1337@gmail.com>
2026-07-09 09:44:19 +0700

    Feat Route

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,1 +1,26 @@
 import { Router } from 'express';
+import ProductController from '../controllers/productController.js';
+import ProductValidation from '../middlewares/productValidation.js';
+
+const ProductRoute = {
+  createRoute: () => {
+    const productRouter = Router();
+    productRouter.get('/', ProductController.getAllProducts);
+    productRouter.get('/:id', ProductController.getProductById);
+    productRouter.post(
+      '/',
+      ProductValidation.validateProduct,
+      ProductController.createProduct,
+    );
+    productRouter.put(
+      '/:id',
+      ProductValidation.validateProduct,
+      ProductController.updateProduct,
+    );
+    productRouter.delete('/:id', ProductController.deleteProduct);
+
+    return productRouter;
+  },
+};
+
+export default ProductRoute;
